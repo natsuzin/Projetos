e1638a43894f51b19c8bee81d179731312df8db0
Nathalia Suzin <98620648+natsuzin@users.noreply.github.com>
2023-05-11 00:21:26 -0300

    Add files via upload

---
+++ calculadora - pilha/main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include "Pilha.h"
+#include "Calculadora.h"
+#include "Validacao.h"
+using namespace std;
+
+int main()
+{
+    string numero, palavra, opcao;
+    float valorAtual, valorAnterior = 0.0, resultado;
+    Pilha<float> pilha;
+
+    inicia(pilha);
+    insere(pilha, valorAnterior);
+
+    do
+    {
+        bool verificado = true;
+        cout<<"-------------------Menu-------------------\n"
+            "Informe por escrito a operacao e o valor entre parenteses:\n"
+            "  ZERA\n"
+            "  SOMA(valor)\n"
+            "  SUBTRAI(valor)\n"
+            "  MULTIPLICA(valor)\n"
+            "  DIVIDE(valor)\n"
+            "  PARCELAS\n"
+            "  IGUAL\n"
+            "  FIM\n"
+            "------------------------------------------\n";
+        getline(cin,opcao);
+        toUpper(opcao);
+        system("cls");
+
+        try
+        {
+            palavra = verificaSeDiferenteDeLetra(opcao);
+        }
+        catch (const char* msg)
+        {
+            cerr << msg << endl;
+            verificado = false;
+        }
+
+        try
+        {
+            numero = verificaSeDiferenteDeNumero(opcao);
+        }
+        catch (const char* msg)
+        {
+            cerr << msg << endl;
+            verificado = false;
+        }
+
+        valorAtual = converteStringParaFloat(numero);
+
+        if(!verificado) cout << "TENTE NOVAMENTE.\n\n";
+        if(verificado)
+        {
+            if(palavra == "ZERA")
+            {
+                zera(pilha);
+                cout << "Pilha (re)inicializada.\n";
+                cout << endl;
+            }
+            if(palavra == "SOMA")
+            {
+                cout << "ADICAO\n";
+                resultado = soma(valorAtual, valorAnterior);
+                valorAnterior = resultado;
+                insere(pilha, resultado);
+                cout << "\nValor adicionado.\n";
+                cout << endl;
+            }
+            if(palavra == "SUBTRAI")
+            {
+                cout << "SUBTRACAO\n";
+                resultado = subtrai(valorAtual, valorAnterior);
+                valorAnterior = resultado;
+                insere(pilha, resultado);
+                cout << "\nValor subtraido.\n";
+                cout << endl;
+            }
+            if(palavra == "MULTIPLICA")
+            {
+                cout << "MULTIPLICACAO\n";
+                resultado = multiplica(valorAtual, valorAnterior);
+                valorAnterior = resultado;
+                insere(pilha, resultado);
+                cout << "\nValor multiplicado.\n";
+                cout << endl;
+            }
+            if(palavra == "DIVIDE")
+            {
+                cout << "DIVISAO\n";
+                resultado = divide(valorAtual, valorAnterior);
+                valorAnterior = resultado;
+                insere(pilha, resultado);
+                cout << "\nValor dividido.\n";
+                cout << endl;
+            }
+            if(palavra == "PARCELAS")
+            {
+                cout << "PARCELAS\n";
+                if(pilha.cardinalidade > 2)
+                    parcelas(pilha);
+                else
+                    cout << "\nNao ha parcelas para apresentar no momento.\n";
+                cout << endl;
+
+            }
+            if(palavra == "IGUAL")
+            {
+                resultado = igual(pilha);
+                cout << "Resultado = " << resultado << endl;
+                cout << endl;
+            }
+            if(palavra == "FIM")
+            {
+                cout << "Saindo...\n";
+                cout << endl;
+            }
+        }
+        system("pause");
+        system("cls");
+    }
+    while(opcao != "FIM");
+
+
+    destroi(pilha);
+
+    return 0;
+}
