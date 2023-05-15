623a3d6e936ddfdf80110b70d4fe8175b032e7c3
Nathalia Suzin <98620648+natsuzin@users.noreply.github.com>
2023-05-14 22:12:44 -0300

    Update Validacao.h

--- calculadora - pilha/Validacao.h
+++ calculadora - pilha/Validacao.h
@@ -1,111 +1,124 @@
-#ifndef VALIDACAO_H_INCLUDED
-#define VALIDACAO_H_INCLUDED
-#include <cstring>
 #include <iostream>
+#include "Pilha.h"
+#include "Calculadora.h"
+#include "Validacao.h"
+using namespace std;
 
-void toUpper(string &aux)
+int main()
 {
-    char a;
-    string aux2="";
-    for(unsigned int i=0; i<aux.size(); i++)
+    string numero, palavra, opcao;
+    float valorAtual, valorAnterior = 0.0, resultado;
+    Pilha<float> pilha;
+
+    inicia(pilha);
+    insere(pilha, valorAnterior);
+
+    do
     {
-        a=toupper(aux.at(i));
-        aux2+=a;
-    }
-    aux=aux2;
-}
+        bool verificado = true;
+        cout<<"-------------------Menu-------------------\n"
+            "Informe por escrito a operacao e o valor entre parenteses:\n"
+            "  ZERA\n"
+            "  SOMA(valor)\n"
+            "  SUBTRAI(valor)\n"
+            "  MULTIPLICA(valor)\n"
+            "  DIVIDE(valor)\n"
+            "  PARCELAS\n"
+            "  IGUAL\n"
+            "  FIM\n"
+            "------------------------------------------\n";
+        getline(cin,opcao);
+        toUpper(opcao);
+        system("cls");
 
-void validaSeVazio(string aux)
-{
-    if(aux.size()==0){
-        throw "INFORME A EXPRESSAO E O VALOR.";
-    }
-}
-
-string verificaSeDiferenteDeNumero(string aux)
-{
-    string numero="";
-    for(unsigned int i=0; i<aux.size(); i++)
-    {
-        if(aux.at(i) == '(')
+        try
         {
-            i++;
-            if(aux.at(i) == ')'){
-                throw "INFORME O VALOR JUNTO A EXPRESSAO.";
-            }
-            for(unsigned int j=i; j<aux.size(); j++)
+            palavra = verificaSeDiferenteDeLetra(opcao);
+        }
+        catch (const char* msg)
+        {
+            cerr << msg << endl;
+            verificado = false;
+        }
+
+        try
+        {
+            numero = verificaSeDiferenteDeNumero(opcao);
+        }
+        catch (const char* msg)
+        {
+            cerr << msg << endl;
+            verificado = false;
+        }
+
+        valorAtual = converteStringParaFloat(numero);
+
+        if(verificado)
+        {
+            if(palavra == "ZERA")
             {
-                if(!isdigit(aux.at(j)))
-                {
-                    if(aux.at(j) == ')')
-                        return numero;
-                    else{
-                        if(aux.at(j) == '.')
-                            numero = numero + aux.at(j);
-                        else{
-                            if(aux.at(j) == '-')
-                                numero = numero + aux.at(j);
-                            else
-                                throw "VALOR INCORRETO.";
-                        }
-                    }
-                }
-                else{
-                    numero = numero + aux.at(j);
-                }
+                zera(pilha);
+                cout << "Pilha (re)inicializada.\n";
+            }
+            if(palavra == "SOMA")
+            {
+                cout << "ADICAO\n";
+                resultado = soma(valorAtual, valorAnterior);
+                valorAnterior = resultado;
+                insere(pilha, resultado);
+                cout << "\nValor adicionado.\n";
+            }
+            if(palavra == "SUBTRAI")
+            {
+                cout << "SUBTRACAO\n";
+                resultado = subtrai(valorAtual, valorAnterior);
+                valorAnterior = resultado;
+                insere(pilha, resultado);
+                cout << "\nValor subtraido.\n";
+            }
+            if(palavra == "MULTIPLICA")
+            {
+                cout << "MULTIPLICACAO\n";
+                resultado = multiplica(valorAtual, valorAnterior);
+                valorAnterior = resultado;
+                insere(pilha, resultado);
+                cout << "\nValor multiplicado.\n";
+            }
+            if(palavra == "DIVIDE")
+            {
+                cout << "DIVISAO\n";
+                resultado = divide(valorAtual, valorAnterior);
+                valorAnterior = resultado;
+                insere(pilha, resultado);
+                cout << "\nValor dividido.\n";
+            }
+            if(palavra == "PARCELAS")
+            {
+                cout << "PARCELAS\n";
+                if(pilha.cardinalidade > 2)
+                    parcelas(pilha);
+                else
+                    cout << "\nNao ha parcelas para apresentar no momento.\n";
+
+            }
+            if(palavra == "IGUAL")
+            {
+                resultado = igual(pilha);
+                cout << "Resultado = " << resultado << endl;
+            }
+            if(palavra == "FIM")
+            {
+                cout << "Saindo...\n";
             }
         }
+        cout << endl;
+        system("pause");
+        system("cls");
     }
-}
+    while(opcao != "FIM");
 
-string verificaSeDiferenteDeLetra(string aux)
-{
-    string palavra = "";
-    for(unsigned int i=0; i<aux.size(); i++)
-    {
-        palavra = palavra + aux.at(i);
-        if(palavra == "ZERA")return palavra;
-        if(palavra == "SOMA")
-        {
-            i++;
-            if(aux.at(i)!='(')
-               throw "COMANDO INVALIDO.";
-            return palavra;
-        }
-        if(palavra == "SUBTRAI")
-        {
-            i++;
-            if(aux.at(i)!='(')
-               throw "COMANDO INVALIDO.";
-            return palavra;
-        }
-        if(palavra == "MULTIPLICA")
-        {
-            i++;
-            if(aux.at(i)!='(')
-               throw "COMANDO INVALIDO.";
-            return palavra;
-        }
-        if(palavra == "DIVIDE")
-        {
-            i++;
-            if(aux.at(i)!='(')
-               throw "COMANDO INVALIDO.";
-            return palavra;
-        }
-        if(palavra == "PARCELAS") return palavra;
-        if(palavra == "IGUAL") return palavra;
-        if(palavra == "FIM") return palavra;
-        if(!isalpha(aux.at(i))) throw "COMANDO INVALIDO.";
-    }
-    throw "COMANDO INVALIDO.";
-}
 
-float converteStringParaFloat(string aux)
-{
-    float valor;
-    valor=atoi(aux.c_str());
-    return valor;
-}
+    destroi(pilha);
 
-#endif // VALIDACAO_H_INCLUDED
+    return 0;
+}
